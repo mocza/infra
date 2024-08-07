7aaa1b7cf832ca3d734fae7c58eff2f0980a346d
mocza <zsolt.mocza@gmail.com>
2024-08-07 04:25:44 +0200

    remove sudo

--- preface.sh
+++ preface.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Run this script right after OS install as a preliminary step before applying automated configuration using Ansible pull.
-#
+# Run this script as the root user right after OS install as a preliminary step before applying automated configuration using Ansible pull.
+# This script is meant to do minimal settings before any other automated configuration to a fresh OS installation can happen:
 # - detects OS: Debian or Arch Linux
 # - detects PVE host and configures apt sources for PVE no-subscription repository
 # - installs pre-requisites for Ansible
@@ -9,15 +9,15 @@
 # Function to configure PVE no-subscription repository
 configure_pve_repo() {
     echo "Configuring Proxmox VE no-subscription repository..."
-    sudo tee /etc/apt/sources.list.d/pve-no-subscription.list <<EOF
+    tee /etc/apt/sources.list.d/pve-no-subscription.list <<EOF
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
 EOF
 
     # Disable the enterprise repository
-    sudo mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.disabled
+    mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.disabled
 
     # Disable the CEPH repository
-    sudo mv /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.disabled
+    mv /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.disabled
 }
 
 # Function to install prerequisites on Debian-based systems
@@ -32,19 +32,19 @@ install_debian() {
 
     # Update package list
     echo "Updating package list..."
-    sudo apt-get update
+    apt-get update
 
     # Install Git
     echo "Installing Git..."
-    sudo apt-get install -y git
+    apt-get install -y git
 
     # Add Ansible PPA and install Ansible
     echo "Adding Ansible PPA..."
-    sudo apt-get install -y software-properties-common
-    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    apt-get install -y software-properties-common
+    apt-add-repository --yes --update ppa:ansible/ansible
 
     echo "Installing Ansible..."
-    sudo apt-get install -y ansible
+    apt-get install -y ansible
 
     # Verify installations
     echo "Verifying installations..."
@@ -60,11 +60,11 @@ install_arch() {
     
     # Update package list and install prerequisites
     echo "Updating package list..."
-    sudo pacman -Syu --noconfirm
+    pacman -Syu --noconfirm
 
     # Install Git and Ansible
     echo "Installing Git and Ansible..."
-    sudo pacman -S --noconfirm git ansible
+    pacman -S --noconfirm git ansible
 
     # Verify installations
     echo "Verifying installations..."
